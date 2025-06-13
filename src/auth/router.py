1be53db8c8f3acdca3f98cc7ac540a35f47e1040
Arimaqqe <beebo120103@gmail.com>
2025-06-13 22:30:21 +0700

    feat(auth): include auth router from fastapi-users

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,11 +1,8 @@
 from fastapi import APIRouter
 
+from auth.config import authentication_backend, fastapi_users
 from config import settings
 
 router = APIRouter(prefix=settings.api_prefix.v1.auth, tags=["Auth"])
 
-
-@router.get("/login")
-def login():
-    return {"message": "Login"}
-
+router.include_router(fastapi_users.get_auth_router(authentication_backend))
