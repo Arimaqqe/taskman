a634e161de7d38cd3622a1a407f5a86ee6e80184
Arimaqqe <beebo120103@gmail.com>
2025-06-13 19:13:28 +0700

    feat(auth): add AccessToken model

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,17 +1,36 @@
 from typing import TYPE_CHECKING
 
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyAccessTokenDatabase,
+    SQLAlchemyBaseAccessTokenTable,
+)
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column
 
-from src.mixins import IdIntMixin
+from src.auth.mixins import IdIntMixin
+from src.auth.types import UserIdType
 from src.models import Base
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(Base, IdIntMixin, SQLAlchemyBaseUserTable[int]):
+class User(Base, IdIntMixin, SQLAlchemyBaseUserTable[UserIdType]):
     pass
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyUserDatabase(session, User)
+        return SQLAlchemyUserDatabase(session, cls)
+
+
+class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[UserIdType]):
+    user_id: Mapped[UserIdType] = mapped_column(
+        Integer,
+        ForeignKey("user.id", ondelete="cascade"),
+        nullable=False,
+    )
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyAccessTokenDatabase(session, cls)
