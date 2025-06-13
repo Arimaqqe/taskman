34d0c27a904771c00cbe226d24baab708e495789
Arimaqqe <beebo120103@gmail.com>
2025-06-13 20:54:47 +0700

    feat(auth): create get database strategy dependency

--- src/auth/config.py
+++ src/auth/config.py
@@ -1,6 +1,29 @@
+from typing import TYPE_CHECKING, Annotated
+
+from fastapi import Depends
 from fastapi_users.authentication import BearerTransport
+from fastapi_users.authentication.strategy.db import DatabaseStrategy
+
+from src.auth.utils import get_access_token_db
+from src.config import settings
+
+if TYPE_CHECKING:
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
+
+    from src.auth.models import AccessToken
 
 bearer_transport = BearerTransport(
     # TODO: update url
     tokenUrl="auth/jwt/login"
+)
+
+
+def get_database_strategy(
+    access_token_db: Annotated[
+        "AccessTokenDatabase[AccessToken]", Depends(get_access_token_db)
+    ],
+):
+    return DatabaseStrategy(
+        database=access_token_db,
+        lifetime_seconds=settings.access_token.lifetime_seconds,
     )
