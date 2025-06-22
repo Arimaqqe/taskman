fd6f69afb137086701eaf9416979b230f51eed93
Arimaqqe <beebo120103@gmail.com>
2025-06-22 14:13:13 +0700

    feat(users): add crud for users

--- src/api.py
+++ src/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from auth.router import router as auth_router
+from auth.router import auth_router, users_router
 from config import settings
 
 router = APIRouter(prefix=settings.api_prefix.v1.prefix)
 
 router.include_router(auth_router)
+router.include_router(users_router)
--- src/auth/router.py
+++ src/auth/router.py
@@ -1,14 +1,19 @@
 from fastapi import APIRouter
 
 from auth.config import authentication_backend, fastapi_users
-from auth.schemas import UserCreate, UserRead
+from auth.schemas import UserCreate, UserRead, UserUpdate
 from config import settings
 
-router = APIRouter(prefix=settings.api_prefix.v1.auth, tags=["Auth"])
+auth_router = APIRouter(prefix=settings.api_prefix.v1.auth, tags=["Auth"])
+users_router = APIRouter(prefix=settings.api_prefix.v1.users, tags=["Users"])
 
 # /login
 # /logout
-router.include_router(fastapi_users.get_auth_router(authentication_backend))
+auth_router.include_router(fastapi_users.get_auth_router(authentication_backend))
 
 # register
-router.include_router(fastapi_users.get_register_router(UserRead, UserCreate))
+auth_router.include_router(fastapi_users.get_register_router(UserRead, UserCreate))
+
+# /me
+# /{id}
+users_router.include_router(fastapi_users.get_users_router(UserRead, UserUpdate))
