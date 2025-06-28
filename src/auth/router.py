99a6875c02d6997e6606c129cd428ed2e6bf3e61
Arimaqqe <beebo120103@gmail.com>
2025-06-28 20:53:00 +0700

    feat(auth): add verify endpoints

--- src/auth/router.py
+++ src/auth/router.py
@@ -14,6 +14,10 @@ auth_router.include_router(fastapi_users.get_auth_router(authentication_backend)
 # register
 auth_router.include_router(fastapi_users.get_register_router(UserRead, UserCreate))
 
+# request-verify-token
+# verify
+auth_router.include_router(fastapi_users.get_verify_router(UserRead))
+
 # /me
 # /{id}
 users_router.include_router(fastapi_users.get_users_router(UserRead, UserUpdate))
